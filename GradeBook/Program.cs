1711ffc596204af3394eb7ce7b22ea19f16fc195
Ryan Silva <RyandotSilva@gmail.com>
2019-09-28 13:00:56 -0400

    Update Book class with AddGrade method

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -8,8 +8,9 @@ namespace GradeBook
         
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Ryan's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
 
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
@@ -19,6 +20,9 @@ namespace GradeBook
             {
                 result += number;
             }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
