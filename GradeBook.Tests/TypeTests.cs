3c0e035e63f207b98d732829baaac309468fb5f5
Ryan Silva <RyandotSilva@gmail.com>
2019-09-29 19:08:27 -0400

    Add WriteLogDelegateCanPointToMethod test

--- GradeBook.Tests/TypeTests.cs
+++ GradeBook.Tests/TypeTests.cs
@@ -4,8 +4,34 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
+    public delegate string WriteLogDelegate(string logMessage);
+
     public class TypeTests
     {
+        private int count = 0;
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log = ReturnMessage;
+            log += ReturnMessage;
+            log += IncrementCount;
+
+            var result = log("Hello");
+            Assert.Equal(3, count);
+        }
+
+        private string IncrementCount(string message)
+        {
+            count++;
+            return message.ToLower();
+        }
+
+        private string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
         [Fact]
         public void StringsBehaveLikeValueTypes()
         {
--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -11,7 +11,7 @@ namespace GradeBook
             Name = name;
         }
 
-        public void AddLetterGrade(char letter)
+        public void AddGrade(char letter)
         {
             switch (letter)
             {
@@ -94,7 +94,13 @@ namespace GradeBook
         }
 
         private List<double> grades;
-        public string Name;
 
+        public string Name 
+        { 
+            get;
+            set;
+        }
+
+        public const string CATEGORY = "Science";
     }
 }
