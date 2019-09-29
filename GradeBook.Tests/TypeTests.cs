f1fb691fa21fd79aa19e0a604475af9ca8c419a2
Ryan Silva <RyandotSilva@gmail.com>
2019-09-29 10:54:17 -0400

    Add TwoVarsCanReferenceSameObject test

---
+++ GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class TypeTests
+    {
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
+        }
+    }
+}
