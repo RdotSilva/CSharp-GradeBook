b99dc1bd204d2dcb17b14c61a7966903b9a873da
Ryan Silva <RyandotSilva@gmail.com>
2019-09-29 14:40:22 -0400

    Add AddLetterGrade method

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -10,6 +10,31 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+                default: AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
             if (grade <= 100 && grade >= 0)
